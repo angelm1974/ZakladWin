29a3c8e23b052be8b9bd61683ab82ff2fdb79a72
angelm1974 <mariusz980@gmail.com>
2025-01-25 12:08:00 +0100

    Dodaj pliki projektów.

---
+++ ZakladWin/frmListaKlientow.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ZakladWin
+{
+    partial class frmListaKlientow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.imie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nazwisko = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.imie,
+            this.nazwisko,
+            this.telefon});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(525, 220);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // imie
+            // 
+            this.imie.HeaderText = "imie";
+            this.imie.Name = "imie";
+            // 
+            // nazwisko
+            // 
+            this.nazwisko.HeaderText = "nazwisko";
+            this.nazwisko.Name = "nazwisko";
+            // 
+            // telefon
+            // 
+            this.telefon.HeaderText = "telefon";
+            this.telefon.Name = "telefon";
+            // 
+            // frmListaKlientow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 244);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmListaKlientow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmListaKlientow";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nazwisko;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefon;
+    }
+}
---
+++ ZakladWin/frmListaKlientow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZakladWin
+{
+    public partial class frmListaKlientow : Form
+    {
+        public frmListaKlientow()
+        {
+            InitializeComponent();
+            frmListaKlientow_Load();
+        }
+
+        private void frmListaKlientow_Load()
+        {
+            foreach (Zaklad.Klient klient in Program.ListaKlientow)
+            {
+                dataGridView1.Rows.Add(klient.Imie, klient.Nazwisko, klient.Telefon);
+            }
+        }
+    }
+}
